cad2449707da9f989bf42ac5cebee1aa1c8c4d55
Eric Laursen <ericjlaursen@gmail.com>
2018-04-11 18:34:33 -0400

    Includes dummy react component.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-console.log(`I'm a silly entry point`);
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log('ES6 Array',...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+  return (
+    <div>
+      <p>Dummy React Component here!</p>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
