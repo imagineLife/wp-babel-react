101baa418fd32782b36be0f057b0da4af40b4c2b
Eric Laursen <ericjlaursen@gmail.com>
2018-04-25 13:39:18 -0400

    Includes basic react div.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>Dummy React Component here!</p>
-    </div>
-  );
-};
-export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+import App from "./App";
