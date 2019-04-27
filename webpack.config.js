ab3a36d3bce2cce361c0cbe6032f31552c8cb1be
Eric Laursen <ericjlaursen@gmail.com>
2019-04-26 21:33:53 -0400

    includes webpcak entry && output

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCss = require("mini-css-extract-plugin");
 
 module.exports = {
+  entry:'./src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
   module: {
     rules: [
       {
