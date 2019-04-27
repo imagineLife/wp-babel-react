5874dfa4f1b839dcb38b4c8a72fca79cfe350271
Eric Laursen <ericjlaursen@gmail.com>
2019-04-27 14:16:28 -0400

    removes comment

--- src/components/Title/index.test.js
+++ src/components/Title/index.test.js
@@ -7,19 +7,12 @@ import Adapter from "enzyme-adapter-react-16"
 configure({ adapter: new Adapter() })
 
 describe('<Title />', () => {
+
 	it('passes address PROP to text', () => {
 		const thisTitle = shallow(<Title text={'testText'}/>)
 		expect(thisTitle.find('h3').text()).toBe('testText')
 	})
-	/*
-		using SetProps
-		https://airbnb.io/enzyme/docs/api/ShallowWrapper/setProps.html#setpropsnextprops-callback--self
-		Takes an object, passes as new props to component
-		causes re-render
-		tests how component will behave with changing props
-		calls componentWillRecieveProps on a class component
-		re-renders on this functional component
-	*/
+	
 	describe('handles conditional render prop-driven updates', () => {
 		it('Updates From firstVal to secondVal', () => {
 			const thisTitle = shallow(<Title text={'firstVal'}/>)
