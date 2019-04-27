7e6118777ea10cf047e0f878f5227154f0de2102
Eric Laursen <ericjlaursen@gmail.com>
2019-04-27 13:51:35 -0400

    functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-class App extends React.Component{
-  
-  state = {
-  	dummyData: "Dummy React component!"
-  }
-  render(){
+const App = ({pText}) => {
   	return (
 	    <div>
-	      <p>{this.state.dummyData}</p>
+	      <p>{pText}</p>
 	    </div>
-	  );	
-  }
-  
+	  );
 };
 export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App pText={'test text'}/>, document.getElementById("app"));
