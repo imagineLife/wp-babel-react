a1c294bc43d9a35e4b20f20671dac35230c8d838
Eric Laursen <ericjlaursen@gmail.com>
2019-06-20 19:34:07 -0400

    includes routing with lazy routes

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom";
 import Title from '../components/Title'
 const DashRoute = React.lazy(() => import('../containers/Dashboard'))
 const DetailRoute = React.lazy(() => import('../containers/ItemDetails'))
+import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-const Router = ({pText}) => {
+
+const AppRouter = () => {
   	return (
-	    <div id="app">
-	    	<p>Router</p>
-	    </div>
+	    <Router>
+	      <Switch>
+	      	
+	      	<Route path="/dashboard" exact render={() => (
+	      	  <React.Suspense fallback={<p>Loading Dashboard...</p>}>
+	      	  	<DashRoute />
+	      	  </React.Suspense>
+	      	)}/>
+
+	      	<Route path="/itemDetails" exact render={() => (
+	      	  <React.Suspense fallback={<p>Loading Item Details...</p>}>
+	      	  	<DetailRoute />
+	      	  </React.Suspense>
+	      	)} />
+
+	      	<Redirect from="/*" to="/dashboard"/>
+	      </Switch>
+	    </Router>
 	  );
 };
 
-export default Router;
+export default AppRouter;
