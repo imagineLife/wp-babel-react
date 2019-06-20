8649eb25efb65daaa0ed9ac50610273035da31ad
Eric Laursen <ericjlaursen@gmail.com>
2019-06-20 18:35:52 -0400

    App folder

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Title from '../components/Title'
+
+const App = ({pText}) => {
+  let titleText = 'app title text!'
+  	return (
+	    <Title text={titleText} />
+	  );
+};
+
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
