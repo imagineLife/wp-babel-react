7f4c588169fd7e9bd9e5f6af3e0c62ec23f5863d
Eric Laursen <ericjlaursen@gmail.com>
2019-06-20 19:09:11 -0400

    introduces lazy loading

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Title from '../components/Title'
+const DashRoute = React.lazy(() => import('../containers/Dashboard'))
+const DetailRoute = React.lazy(() => import('../containers/ItemDetails'))
 
 const Router = ({pText}) => {
   	return (
