39366c10d7a81876140bf1431e0f7f221672977d
Eric Laursen <ericjlaursen@gmail.com>
2019-06-21 06:27:19 -0400

    fetches appConfig on load

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Title from '../components/Title'
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import fetcher from '../../utils/fetcher';
+
 const DashRoute = React.lazy(() => import('../containers/Dashboard'))
 const DetailRoute = React.lazy(() => import('../containers/ItemDetails'))
 
 const AppRouter = () => {
+
+	let [ appConfig, setAppConfig ] = React.useState(null)
+	/*
+		"onLoad", load the appconfig file
+	*/
+	React.useEffect(() => {
+		fetch(`./appconfig/config.js`)
+		.then(res => res.json()) 
+		.then(res => {
+			setAppConfig(res[0]['dev'])	
+		})
+
+	}, [])
+
+	console.log('appConfig')
+	console.log(appConfig)
+	
+	
   	return (
 	    <Router>
 	      <Switch>
