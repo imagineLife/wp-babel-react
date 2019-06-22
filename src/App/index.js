9eacc95210394f389d150ede553ccfe09db01ebd
Eric Laursen <ericjlaursen@gmail.com>
2019-06-22 15:30:14 -0400

    imports login, conditional-render from root

--- src/App/index.js
+++ src/App/index.js
@@ -4,12 +4,14 @@ import Title from '../components/Title'
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import { useAppConfig } from '../lib/useAppConfig'
 
+const LoginRoute = React.lazy(() => import('../containers/Login'))
 const DashRoute = React.lazy(() => import('../containers/Dashboard'))
 const DetailRoute = React.lazy(() => import('../containers/ItemDetails'))
 
 const AppRouter = () => {
 	const appConfig = useAppConfig()
 	const [srcData, setSrcData] = React.useState(null)
+	const [userData, setUserData] = React.useState(null)
 
 	React.useEffect(() => {
 		if(appConfig !== null){
@@ -37,19 +39,33 @@ const AppRouter = () => {
 	    <Router>
 	      <Switch>
 	      	
-	      	<Route path="/dashboard" exact render={() => (
+	      	<Route exact path="/login" exact render={() => (
+	      	  <React.Suspense fallback={<p>Loading Login...</p>}>
+	      	  	<LoginRoute />
+	      	  </React.Suspense>
+	      	)}/>
+	      	<Route exact path="/dashboard" exact render={() => (
 	      	  <React.Suspense fallback={<p>Loading Dashboard...</p>}>
-	      	  	<DashRoute />
+	      	  	<DashRoute data={srcData} />
 	      	  </React.Suspense>
 	      	)}/>
 
-	      	<Route path="/itemDetails" exact render={() => (
+	      	<Route exact path="/itemDetails" exact render={() => (
 	      	  <React.Suspense fallback={<p>Loading Item Details...</p>}>
 	      	  	<DetailRoute />
 	      	  </React.Suspense>
 	      	)} />
 
-	      	<Redirect from="/*" to="/dashboard"/>
+	      	<Route exact path="/" render={() => (
+			  userData ? (
+			    <Redirect to="/dashboard"/>
+			  ) : (
+			    <Redirect to="/login"/>
+			  )
+			)}/>
+
+	      	<Redirect from="/*" to="/dashboard" />
+
 	      </Switch>
 	    </Router>
 	  );
