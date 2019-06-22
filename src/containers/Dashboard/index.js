5e3f60a7f7432acdea5be836a4fec46927148483
Eric Laursen <ericjlaursen@gmail.com>
2019-06-22 15:19:57 -0400

    imports d3-scale && begins building xScale

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,6 +1,12 @@
 import React from "react"
+import {scaleOrdinal} from 'd3-scale';
 
-const Dashboard = () => {
+const Dashboard = ({data}) => {
+
+	let xScale = scaleOrdinal().domain(data.map(d => {
+		return Object.keys(d)[0]
+	}))
+	
 	return(
 		<p>Dashboard here</p>
 	)
