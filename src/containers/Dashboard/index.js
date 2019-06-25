0a13df63023f3a7bccee51f7c0ec5324fadae5f3
Eric Laursen <ericjlaursen@gmail.com>
2019-06-25 10:00:09 -0400

    renders simple bar

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import {scaleOrdinal} from 'd3-scale';
+import { scaleOrdinal, scaleLinear, scaleBand } from 'd3-scale';
 import { Redirect } from 'react-router-dom'
- 
+import BarChart from '../../components/BarChart'
 const Dashboard = ({data, loggedIn}) => {
 
 	if(loggedIn !== true){
-		console.log('HERE!!!!!');
+		console.log('Dashbaord Loaded...');
 		return( <Redirect to="/login" /> )
 	}
 
@@ -13,13 +13,24 @@ const Dashboard = ({data, loggedIn}) => {
 		return(<p>No Data Yet...</p>)
 	}
 	
-
-	let xScale = scaleOrdinal().domain(data.map(d => {
+	let xScale = scaleBand().domain(data.map(d => {
 		return Object.keys(d)[0]
-	}))
+	})).range([0, 400])
+	.paddingInner(.2)
+	.paddingOuter(.1)
+	
+
+	let yScale = scaleLinear().domain([0, 20]).range([0, 250])
 
 	return(
-		<p>Dashboard here</p>
+		<React.Fragment>
+			<p>Dashboard here</p>
+			<BarChart
+				data={data}
+				xScale={xScale}
+				yScale={yScale} />
+		</React.Fragment>
+
 	)
 }
 
