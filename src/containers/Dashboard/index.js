0277129f6ad01f788aad94859aa2dff85da271e9
Eric Laursen <ericjlaursen@gmail.com>
2019-06-24 20:42:52 -0400

    Includes redirec if not logged in

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,12 +1,22 @@
 import React from "react"
 import {scaleOrdinal} from 'd3-scale';
+import { Redirect } from 'react-router-dom'
+ 
+const Dashboard = ({data, loggedIn}) => {
 
-const Dashboard = ({data}) => {
+	if(loggedIn !== true){
+		console.log('HERE!!!!!');
+		return( <Redirect to="/login" /> )
+	}
+
+	console.log('data')
+	console.log(data)
+	
 
 	let xScale = scaleOrdinal().domain(data.map(d => {
 		return Object.keys(d)[0]
 	}))
-	
+
 	return(
 		<p>Dashboard here</p>
 	)
