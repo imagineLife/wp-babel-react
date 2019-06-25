cd05b3e6a607958c67970e06fd6ec0056cf8b066
Eric Laursen <ericjlaursen@gmail.com>
2019-06-25 10:23:45 -0400

    conditional xScale based on data

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -8,27 +8,26 @@ const Dashboard = ({data, loggedIn}) => {
 		console.log('Dashbaord Loaded...');
 		return( <Redirect to="/login" /> )
 	}
-
-	if(!data){
-		return(<p>No Data Yet...</p>)
+	
+	let xScale;
+	if(data){
+		xScale = scaleBand().domain(data.map(d => {
+			return Object.keys(d)[0]
+		})).range([0, 400])
+		.paddingInner(.2)
+		.paddingOuter(.1)
 	}
-	
-	let xScale = scaleBand().domain(data.map(d => {
-		return Object.keys(d)[0]
-	})).range([0, 400])
-	.paddingInner(.2)
-	.paddingOuter(.1)
-	
 
 	let yScale = scaleLinear().domain([0, 20]).range([0, 250])
 
 	return(
 		<React.Fragment>
 			<p>Dashboard here</p>
+			
 			<BarChart
 				data={data}
 				xScale={xScale}
-				yScale={yScale} />
+				yScale={yScale}/>
 		</React.Fragment>
 
 	)
