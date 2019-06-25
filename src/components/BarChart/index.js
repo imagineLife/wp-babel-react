a0b1d8810a8e8c85b1fdd0a462bc9220ab96f213
Eric Laursen <ericjlaursen@gmail.com>
2019-06-24 22:06:09 -0400

    simple bar

---
+++ src/components/BarChart/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css'
+const BarChart = ({data, xScale, yScale}) => {
+	return(
+		<svg 
+			id="barChartWrapper" 
+			width="400" 
+			height="250">
+			{data.map((d, ind) => {
+		
+				let thisKey = Object.keys(d)[0]
+						
+				return(
+					<rect 
+						key={`${thisKey}${ind}`} 
+						x={xScale(thisKey)} 
+						y={yScale(d[thisKey])}
+						height={400 - yScale(d[thisKey])}
+						fill={'steelblue'}
+						width={xScale.bandwidth()}/>
+				)
+			})}
+		</svg>
+	)
+}
+
+export default BarChart;
