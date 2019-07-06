03fc1842d25e84dfe18d192f3f1a1ef0a2bae906
Eric Laursen <ericjlaursen@gmail.com>
2019-07-06 13:45:50 -0400

    includes heavy-html skeleton

--- src/containers/responsiveBoxes/index.js
+++ src/containers/responsiveBoxes/index.js
@@ -3,9 +3,87 @@ import './index.css'
 
 const ResponsiveBoxes = () => {
   return(
-    <div id="responsive-boxes">
-      <h2>Responsive Boxes Here</h2>
-    </div>)
+    <main role="main">
+
+    <div className="row">
+      <div className="col-2">
+        <div className="box orange">2 columns</div>
+      </div>
+      <div className="col-2">
+        <div className="box blue">2 columns</div>
+      </div>
+      <div className="col-2">
+        <div className="box green">2 columns</div>
+      </div>
+      <div className="col-2">
+        <div className="box black">2 columns</div>
+      </div>
+      <div className="col-2">
+        <div className="box black">2 columns</div>
+      </div>
+      <div className="col-2">
+        <div className="box black">2 columns</div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-2-3">
+        <div className="box orange">2-3 columns</div>
+      </div>
+      <div className="col-2-3">
+        <div className="box blue">2-3 columns</div>
+      </div>
+      <div className="col-2-3">
+        <div className="box green">2-3 columns</div>
+      </div>
+      <div className="col-2-3">
+        <div className="box black">2-3 columns</div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-3">
+        <div className="box orange">3 columns</div>
+      </div>
+      <div className="col-3">
+        <div className="box blue">3 columns</div>
+      </div>
+      <div className="col-3">
+        <div className="box green">3 columns</div>
+      </div>
+      <div className="col-3">
+        <div className="box black">3 columns</div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-4">
+        <div className="box orange">4 columns</div>
+      </div>
+      <div className="col-4">
+        <div className="box blue">4 columns</div>
+      </div>
+      <div className="col-4">
+        <div className="box green">4 columns</div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-6">
+        <div className="box orange">6 columns</div>
+      </div>
+      <div className="col-6">
+        <div className="box blue">6 columns</div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-12">
+        <div className="box orange">12 columns</div>
+      </div>
+    </div>
+
+  </main>)
 }
 
 export default ResponsiveBoxes;
