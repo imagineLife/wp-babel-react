e63b9e7ccbb68e082906e6402b358e64c68e793e
Eric Laursen <ericjlaursen@gmail.com>
2019-07-06 14:08:14 -0400

    adds ref to each box

--- src/containers/responsiveBoxes/SingleBox/index.js
+++ src/containers/responsiveBoxes/SingleBox/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const SingleBox = ({boxWrapperClass, boxClassName, boxTxt}) => {
+
+	const boxRef = React.useRef(null);
+
+	React.useEffect(()=> {
+		console.log('boxRef.current')
+		console.log(boxRef.current)
+		
+	})
+
 	return(<div className={boxWrapperClass}>
-	  <div className={`box ${boxClassName}`}>{boxTxt}</div>
+	  <div 
+	  	className={`box ${boxClassName}`}
+	  	ref={boxRef}
+	  	>{boxTxt}</div>
 	</div>)
 } 
 
