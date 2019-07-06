ea866c08d91b482dceadcae058e646902ef1088f
Eric Laursen <ericjlaursen@gmail.com>
2019-07-06 15:41:35 -0400

    uses dims from porps

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './index.css'
-const BarChart = ({data, xScale, yScale}) => {
+const BarChart = ({data, xScale, yScale, dims}) => {
+
+	let [usableDims, setUsableDims] = React.useState({w: dims.width * .75, h: dims.height * .5})
+	
 	return(
 		<svg 
 			id="barChartWrapper" 
-			width="400" 
-			height="250">
+			width={usableDims.w} 
+			height={usableDims.h}>
+
+			{/* If No Data, show placeholder text*/}
+
 			{!data && <text x="50" y="50">loading data...</text>}
 			
+			{/* If data, show bars */}
 			{data && data.map((d, ind) => {
 		
 				let thisKey = Object.keys(d)[0]
