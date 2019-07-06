f3a42cf127cb3e5935f7df9c04bceec213b96c7f
Eric Laursen <ericjlaursen@gmail.com>
2019-07-06 13:43:16 -0400

    responsive route skeleton, including styling

---
+++ src/containers/responsiveBoxes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+
+const ResponsiveBoxes = () => {
+  return(
+    <div id="responsive-boxes">
+      <h2>Responsive Boxes Here</h2>
+    </div>)
+}
+
+export default ResponsiveBoxes;
