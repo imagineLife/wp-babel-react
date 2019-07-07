26ad14388aa8e212726d18099461deb9eee92deb
Eric Laursen <ericjlaursen@gmail.com>
2019-07-07 10:10:11 -0400

    introduces rtg

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import './index.css'
+
+import { CSSTransition } from 'react-transition-group';
+
 const BarChart = ({data, xScale, yScale, dims}) => {
-	
+
 	return(
 		<svg 
 			id="bar-chart-wrapper" 
 			width={dims.w} 
 			height={dims.h}>
-
-			{/* If No Data, show placeholder text*/}
-
-			{!data && <text x="50" y="50">loading data...</text>}
 			
+
 			{/* If data, show bars */}
-			{data && data.map((d, ind) => {
-		
+			{data && 
+			  data.map((d, ind) => {
+
 				let thisKey = Object.keys(d)[0]
 						
 				return(
-					<rect 
-						className="single-bar"
-						key={`${thisKey}${ind}`} 
+				  <CSSTransition
+				  	key={`${thisKey}${ind}`} 
+					component="g"
+					timeout={1550}
+					in={data && data.length > 0}
+					classNames="single-rects"
+					appear
+					onEnter={() => console.log('enter!')}
+					onEntering={() => console.log('entering!')}
+					onEntered={() => console.log('entered!')}>
+					  <rect
 						x={xScale(thisKey)} 
 						y={yScale(d[thisKey])}
 						height={dims.h - yScale(d[thisKey])}
 						fill={'steelblue'}
-						width={xScale.bandwidth()}/>
+						width={xScale.bandwidth()} />
+					</CSSTransition>	
 				)
-			})}
+			  })}
+
+			  {/* If No Data, show placeholder text*/}
+			  {!data && <text x="50" y="50">loading data...</text>}
+			}
 		</svg>
 	)
 }
